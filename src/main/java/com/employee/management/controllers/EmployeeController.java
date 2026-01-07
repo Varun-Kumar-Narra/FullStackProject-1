ee0dd9eb399a5d8168e12525a3e8db7ab870055c
Varun-Kumar-Narra <varun.narra1480@gmail.com>
2026-01-07 11:10:46 -0600

    All the local files are committed

---
+++ src/main/java/com/employee/management/controllers/EmployeeController.java
@@ -0,0 +1,66 @@
+package com.employee.management.controllers;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.employee.management.repositories.EmployeeRepository;
+import com.employee.management.entities.Employee;
+import com.employee.management.exceptions.EmployeeNotFoundException;
+
+@RestController
+@CrossOrigin(origins="http://localhost:4200")
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		return employeeRepository.findAll();
+	}
+	
+	@PostMapping("/employees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+	
+	@GetMapping("/employees/{empId}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Integer empId) {
+		Employee employee = employeeRepository.findById(empId).orElseThrow(()-> new EmployeeNotFoundException("Employee with EmpId : "+empId+"is not found"));
+		return ResponseEntity.ok(employee);
+	}
+	
+	@PutMapping("/employees/{empId}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Integer empId, @RequestBody Employee employeeDetails) {
+		Employee employee = employeeRepository.findById(empId).orElseThrow(()-> new EmployeeNotFoundException("Employee with EmpId : "+empId+"is not found"));
+		employee.setEmpName(employeeDetails.getEmpName());
+		employee.setDesignation(employeeDetails.getDesignation());
+		employee.setEmpSalary(employeeDetails.getEmpSalary());
+		employeeRepository.save(employee);
+		return ResponseEntity.ok(employee);
+	}
+	
+	@DeleteMapping("/employees/{empId}")
+	public ResponseEntity<Map<String,Boolean>> deleteEmployee(@PathVariable Integer empId) {
+		Employee employee = employeeRepository.findById(empId).orElseThrow(()-> new EmployeeNotFoundException("Employee with EmpId : "+empId+"is not found"));
+		employeeRepository.delete(employee);
+		Map<String,Boolean> response = new HashMap<String, Boolean>();
+		response.put("Deleted",Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+
+	
+
+}
